f210405ea81baf99d307d8d609dc3269cabbd89f
Marcelo Matos <marcelomatos.dev@gmail.com>
2020-05-21 00:03:14 -0500

    gitignore

---
+++ build/precache-manifest.b8051864249861c32004940047916ba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b882794bcde35a961fa97e86686c8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25afd214612067276b7a",
+    "url": "/static/js/2.8b9fbc0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8b9fbc0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af7af28a50c698e5046",
+    "url": "/static/js/main.c6d9b95e.chunk.js"
+  },
+  {
+    "revision": "5399181bf48f2256e3db",
+    "url": "/static/js/runtime-main.49be5b6a.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
