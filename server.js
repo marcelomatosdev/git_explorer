289ed17137aeb88e2b35f409debcf3bf0052820d
Marcelo Matos <marcelomatos.dev@gmail.com>
2020-05-20 22:52:06 -0500

    .env file created2

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+app.listen(port, () => {
+   console.log(`Server is up on port ${port}!`);
+});
